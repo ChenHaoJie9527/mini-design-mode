3b3ddc407f7f323f24477ab95c05ef6e3f4fafd9
tourl-ts <18312883857@163.com>
2023-01-24 22:03:43 +0800

    feat:

--- src/design/prototypePattern.ts
+++ src/design/prototypePattern.ts
@@ -1,3 +1,28 @@
-/**
- * 原型模式
- */
+class Prototype {
+  primitive: any;
+  component: object;
+  circularReference: ComponentWithBackReference;
+
+  clone(): this {
+    const clone = Object.create(this);
+    clone.component = Object.create(this.component);
+
+    clone.circularReference = {
+      ...this.circularReference,
+      prototype: {
+        ...this,
+      },
+    };
+
+    return clone;
+  }
+}
+
+class ComponentWithBackReference {
+  public prototype;
+  constructor(prototype: Prototype) {
+    this.prototype = prototype;
+  }
+}
+
+export {};
